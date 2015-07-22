7ab2dcbaf6c7f279c862d251befa62f2c5ec4d42
Nithya Rao <bumblebeenr@gmail.com>
2015-07-22 11:11:59 -0500

    InProgress blocitoff-Mark-ToDo-Items-Complete before checkmark alignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,12 +7,27 @@ class ItemsController < ApplicationController
     @item.user = @user
     if @item.save
        flash[:notice] = "Item was saved to the To-Do list"
-       redirect_to [@user]
+       redirect_to @user
      else
        flash[:error] = "There was an error saving the item. Please try again."
        render "items/new" 
      end       
    end
+
+  def destroy
+     @item = Item.find(params[:id])
+    # authorize @item
+     if @item.destroy
+      flash[:notice] = "\"#{@item.name}\" was deleted successfully."
+       redirect_to @user
+     else
+       flash[:error] = "There was an error deleting the Item. Please try again."
+       render user_path[@user]
+     end
+  end
+
+
+  private
 
   def item_params
      params.require(:item).permit(:name)
