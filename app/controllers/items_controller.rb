296991a8c4ee738ad79548cf7ee6dc31c8471be5
Nithya Rao <bumblebeenr@gmail.com>
2015-07-26 10:13:30 -0500

    Completd blocitoff-Display-Time-Remaining without Rspec test

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,14 +3,14 @@ class ItemsController < ApplicationController
  def create
     @user = User.find(params[:user_id])
     @items = @user.items
-    @item = @user.items.build(item_params)
-    @item.user = @user
+    @item = Item.new(item_params)
+    @item.user = current_user
+    @new_item = Item.new
     if @item.save
        flash[:notice] = "Item was saved to the To-Do list"
     else
        flash[:error] = "There was an error saving the item. Please try again."
     end 
-
      respond_to do |format|
       format.html { redirect_to [@item.user] }
       format.js
